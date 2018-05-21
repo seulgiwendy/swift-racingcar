3a090d5f40bfd5ef7cfaf3e74834e633c5d501da
seulgiwendy <wheejuni@gmail.com>
2018-05-21 13:25:24 +0900

    Initial Commit

---
+++ racingcar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  racingcar
+//
+//  Created by wheejuni on 2018. 5. 21..
+//  Copyright © 2018년 wheejuni. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
